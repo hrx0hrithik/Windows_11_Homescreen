3076f78fcabffeaa2ef65ebcb7155da029962be3
hrx0hrithik <hrithikgupta52@gmail.com>
2023-03-21 03:32:15 +0530

    updated preloader

--- index.js
+++ index.js
@@ -26,19 +26,67 @@ var time = new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', m
 console.log(time);
 document.getElementById("time").textContent = time;
 
-var date = new Date().toLocaleDateString();
-console.log(date);
-document.getElementById("date").textContent = date;
+const monthArr = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// var date = new Date().toLocaleDateString();
+let objectDate = new Date();
+let day = objectDate.getDate();
+// console.log(day);
+
+let month = objectDate.getMonth();
+let monthName = monthArr[objectDate.getMonth()];
+// console.log(month + 1); 
+
+let year = objectDate.getFullYear();
+// console.log(year);
+if (day < 10) {
+    day = '0' + day;
+}
+
+if (month < 10) {
+    month = `0${month}`;
+}
+
+let formatDate = `${day}-${month}-${year}`;
+console.log(formatDate);
+document.getElementById("date").textContent = formatDate;
+document.querySelector(".time").title = `${day} ${monthName} ${year}`;
 
 const wifiBtn = document.querySelector('#wifi-click');
 const btBtn = document.querySelector('#bt-click');
+const wifi1 = document.querySelector('.wifi-btn');
+const wifi2 = document.querySelector('#wifi-mor')
+const bt1 = document.querySelector('.bt-btn');
+const bt2 = document.querySelector('#bt-mor');
 
 wifiBtn.addEventListener('click', function(){
-    console.log("clicked");
-    if(settingPanel.style.background_Color = "blue"){
-        settingPanel.style.background_Color = "white";
-    }
-    else{
-        settingPanel.style.background_Color = "blue";
-    }
+    // console.log("clicked");
+    wifi1.classList.toggle('active-btn');
+    wifi2.classList.toggle('active-btn');
+})
+
+btBtn.addEventListener('click', function(){
+    bt1.classList.toggle('active-btn');
+    bt2.classList.toggle('active-btn');
+})
+
+const airBtn = document.querySelector('#air-btn');
+const battBtn = document.querySelector('#batt-save');
+const nightBtn = document.querySelector('#night');
+const accessBtn = document.querySelector('#access');
+
+airBtn.addEventListener('click', function(){
+    airBtn.classList.toggle('active-btn');
+
+})
+battBtn.addEventListener('click', function(){
+    battBtn.classList.toggle('active-btn');
+
+})
+nightBtn.addEventListener('click', function(){
+    nightBtn.classList.toggle('active-btn');
+
+})
+accessBtn.addEventListener('click', function(){
+    accessBtn.classList.toggle('active-btn');
+
 })
